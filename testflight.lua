e2ba0463f75e0c57339e0e20975d3a4fdcd0ffc3
Arkiver2 <Arkiver@hotmail.com>
2015-02-13 17:56:45 +0100

    testflight.lua: skip 410 .plist url after 5 tries

--- testflight.lua
+++ testflight.lua
@@ -107,7 +107,22 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     end
   end
   
-  if status_code >= 500 or
+  if string.match(url["url"], "https://testflightapp.com/api/builds/[^/]+/6280ca3ee10631fd6817100ffd1ee849-MTMzMzc3.plist") and status_code == 410 then
+    io.stdout:write("Server returned "..http_stat.statcode..". Sleeping.\n")
+    io.stdout:flush()
+
+    os.execute("sleep 5")
+
+    tries = tries + 1
+
+    if tries >= 5 then
+      io.stdout:write("Skip this url: "..url["url"].."\n")
+      io.stdout:flush()
+      return wget.actions.EXIT
+    else
+      return wget.actions.CONTINUE
+    end
+  elseif status_code >= 500 or
     (status_code >= 400 and status_code ~= 404) then
     io.stdout:write("\nServer returned "..http_stat.statcode..". Sleeping.\n")
     io.stdout:flush()
